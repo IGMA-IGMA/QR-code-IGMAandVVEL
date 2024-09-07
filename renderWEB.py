ee73545b30e23da54723083837aa585c6df7c140
IGMA <i9296517650@yandex.ru>
2024-09-07 22:06:46 +0300

    Velichko COMIT

--- renderWEB.py
+++ renderWEB.py
@@ -4,12 +4,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/Home Page")
+def home_page():
+    return render_template("HomePage.html", title="Home Page")
 
-@app.route("/about")
-def about():
-    return "<h1> О сайте </h1>"
+@app.route("/Creating QR-code")
+def creating():
+    return render_template("creatingQR-code.html")
+
+@app.route("/Recognize QR-code")
+def recognize():
+    return render_template("recognizeQR-code.html")
+
+@app.errorhandler(404)
+def erroe404(error):
+    return render_template('page404.html', error=error)
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
